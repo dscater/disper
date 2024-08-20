4993e03430028ac48ffc48fdd792b10b3fabaf16
dscater <victorgonzalo_as@hotmail.com>
2024-08-20 12:50:54 -0400

    Correccion archivo MLEntrenamientoController

--- app/Http/Controllers/MLEntrenamientoController.php
+++ app/Http/Controllers/MLEntrenamientoController.php
@@ -4,44 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Lugar;
 use Illuminate\Http\Request;
-use Phpml\Clustering\KMeans;
 use Phpml\ModelManager;
-use Phpml\Estimator;
+use Phpml\Clustering\KMeans;
 
 class MLEntrenamientoController extends Controller
 {
 
     public function entrenamientoModelo()
     {
+        // Obtener todos los lugares
         $lugares = Lugar::all();
-        // crear la información necesaria con la capacidad del lugar
-        // deacuerdo a este se asignara una cierta cantidad de personal al lugar
+
+        // Crear la información necesaria con la capacidad del lugar
         $data = [];
-        $lugares = [];
+        $nombresLugares = [];
         foreach ($lugares as $key_lugar => $item) {
             $data[] = [
                 "capacidad" => $item->capacidad,
             ];
-            // agregamos $key_lugar para diferenciar el lugar de la zona ya que en algunas zonas
-            // existen mas de una latitud y longitud en el mapa 
-            $lugares[] = $item->nombre . $key_lugar;
+            // Agregar $key_lugar para diferenciar el lugar en caso de zonas con más de una latitud y longitud
+            $nombresLugares[] = $item->nombre . $key_lugar;
         }
 
-        // Convertimos los datos en un array para las características (X)
+        // Convertir los datos en un array para las características (X)
         $samples = [];
         foreach ($data as $item) {
-            $samples[] = [$item['Capacidad']];
+            $samples[] = [$item['capacidad']];
         }
 
-        // Creamos y entrenamos el modelo
-        $total_lugares = count($lugares);
-        $clusterer = new Estimator($total_lugares);
-        // pasamos la capacidad y lugares respectivamente
-        $clusterer->train($samples, $lugares);
+        // Usar KMeans para clustering
+        $totalClusters = count($nombresLugares); // O puedes definir un número fijo de clusters
+        $kmeans = new KMeans($totalClusters);
 
-        // Guardamos el modelo entrenado para su uso posterior
+        // Entrenar el modelo
+        $clusters = $kmeans->cluster($samples);
+
+        // Guardar el modelo entrenado para su uso posterior
         $modelManager = new ModelManager();
-        $modelManager->saveToFile($clusterer, public_path("modelos/modelo_entrenado.phpml"));
+        $modelManager->saveToFile($kmeans, public_path("modelos/modelo_entrenado.phpml"));
 
         return true;
     }
